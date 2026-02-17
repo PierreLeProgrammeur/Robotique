b41dc5365f21e535cbc8858e9e0b89165383faa3
Shinibarty <albanstievenard59@gmail.com>
2026-02-17 15:31:40 +0100

    Fin du TP1 (moteurs etc)

--- project/main.py
+++ project/main.py
@@ -1,5 +1,12 @@
+import math
 from robot.robot_mobile import RobotMobile
-robot = RobotMobile()
+from robot.moteur import *
+
+moteur_diff = MoteurDifferentiel()
+robot = RobotMobile(moteur=moteur_diff)
+dt = 1.0 # pas de temps (s)
 robot.afficher()
-robot.avancer(1.0)
+# On doit nommer les arguments (v = ..., omega = ...) car on utilise **kwargs !
+robot.commander(v = 1.0, omega = 0.0) # avance
+robot.mettre_a_jour(dt)
 robot.afficher()
--- project/robot/robot_mobile.py
+++ project/robot/robot_mobile.py
@@ -1,10 +1,11 @@
 import math
 
 class RobotMobile:
-    def __init__(self, x=0.0, y=0.0, theta=0.0):
+    def __init__(self, x=0.0, y=0.0, theta=0.0, moteur=None):
         self.x = float(x)
         self.y = float(y)
         self.theta = float(theta)
+        self.moteur = moteur
 
     def position(self):
         return self.x, self.y
@@ -12,6 +13,10 @@ class RobotMobile:
     def afficher(self):
         print(self.x, self.y, self.theta)
 
-    def avancer(self, distance):
-        self.x += distance * math.cos(self.theta)
-        self.y += distance * math.sin(self.theta)
+    def commander(self, **kwargs):
+        if self.moteur is not None:
+            self.moteur.commander(**kwargs)
+
+    def mettre_a_jour(self, dt):
+        if self.moteur is not None:
+            self.moteur.mettre_a_jour(self, dt)
